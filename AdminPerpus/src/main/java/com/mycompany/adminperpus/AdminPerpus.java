238133e3f5cd74e9f8a436a2e7baa1cd55bdfa6a
tbhan <tbhan@192.168.56.1>
2023-11-09 12:07:10 +0700

    test push

---
+++ AdminPerpus/src/main/java/com/mycompany/adminperpus/AdminPerpus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adminperpus;
+
+/**
+ *
+ * @author tbhan
+ */
+public class AdminPerpus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone!");
+    }
+}
